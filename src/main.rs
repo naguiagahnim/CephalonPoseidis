3fd6ea119e92e159f28580c9825cd469d5b32602
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-02-14 11:36:45 +0100

    setup basique

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::async_trait;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+        
+        if let Some(guild) = ready.guilds.first() {
+            if let Some(channel) = guild.id.channels(&ctx.http).await.unwrap().values().next() {
+                if let Err(why) = channel.id.say(&ctx.http, "Hello, World!").await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = std::env::var("DISCORD_TOKEN").expect("Token non trouvé");
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
